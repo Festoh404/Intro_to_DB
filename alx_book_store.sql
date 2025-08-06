21924c1cb6c40d3bd1b396ac24fae7cf1d89cb67
Festus <savalifestus82@gmail.com>
2025-08-06 23:44:30 +0300

    Update alx_book_store.sql to include correct Order Details table name

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,7 +22,7 @@ CREATE TABLE Orders(
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
-CREATE TABLE Order_DETAILS(
+CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
